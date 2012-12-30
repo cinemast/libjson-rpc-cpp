621533732c20010a1e9f55aabed1e6cf8a211ae3
Peter Spiess-Knafl <peter.knafl@bitmovin.net>
2012-12-31 00:52:08 +0100

    added errors

---
+++ lib/jsonrpc/errors.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file errors.cpp
+ * @date 31.12.2012
+ * @author Peter Spiess-Knafl <peter.knafl@gmail.com>
+ * @brief to be defined
+ */
+
+#include "errors.h"
+
+namespace jsonrpc
+{
+    static Errors* instance = NULL;
+    
+    Errors::Errors()
+    {
+        //Official Errors
+        this->possibleErrors[ERROR_INVALID_JSON_REQUEST] =
+                "INVALID_JSON_REQUEST: The JSON sent is not a valid JSON-RPC Request object";
+        this->possibleErrors[ERROR_METHOD_NOT_FOUND] =
+                "METHOD_NOT_FOUND: The method being requested is not available on this server";
+        this->possibleErrors[ERROR_INVALID_PARAMS] =
+                "INVALID_PARAMS: Invalid method parameters (invalid name and/or type) recognised";
+        this->possibleErrors[ERROR_JSON_PARSE_ERROR] =
+                "JSON_PARSE_ERROR: The JSON-Object is not JSON-Valid";
+        this->possibleErrors[ERROR_INTERNAL_ERROR] = "INTERNAL_ERROR: ";
+
+        //Specific Errors
+        this->possibleErrors[ERROR_PROCEDURE_IS_METHOD] =
+                "PROCEDURE_IS_METHOD: The requested notification is declared as a method";
+        this->possibleErrors[ERROR_PROCEDURE_IS_NOTIFICATION] =
+                "PROCEDURE_IS_NOTIFICATION: The requested method is declared as notification";
+        this->possibleErrors[ERROR_PROCEDURE_PARSE_ERROR] =
+                "PROCEDURE_PARSE_ERROR: Something went wrong during parsing a procedure-signature";
+        this->possibleErrors[ERROR_PROCEDURE_POINTER_IS_NULL] =
+                "PROCEDURE_POINTER_IS_NULL: Server has no function Reference registered";
+        this->possibleErrors[ERROR_PERMISSION_DENIED] =
+                "PERMISSION_DENIED: Insufficient permissions for this request";
+        this->possibleErrors[ERROR_MALLFORMED_AUTHENTICATION_HEADER] =
+                "MALLFORMED_AUTHENTICATION_HEADER: Authentication header has wrong format";
+    }
+    
+    Errors::~Errors()
+    {
+        this->possibleErrors.clear();
+    }
+
+    Errors* Errors::GetInstance()
+    {
+        if (instance == NULL)
+        {
+            instance = new Errors();
+        }
+        return instance;
+    }
+
+    bool Errors::AddError(int errorCode, const std::string& errorMessage)
+    {
+        if (errorCode < SPECIFIC_ERROR_STOP || errorCode > SPECIFIC_ERROR_START)
+        {
+            return false;
+        }
+        else
+        {
+            this->possibleErrors[errorCode] = errorMessage;
+            return true;
+        }
+    }
+
+    Json::Value Errors::GetErrorBlock(int errorCode)
+    {
+        Json::Value error;
+        error[KEY_ERROR_CODE] = errorCode;
+        error[KEY_ERROR_MESSAGE] = this->possibleErrors[errorCode];
+        return error;
+    }
+
+} /* namespace jsonrpc */
---
+++ lib/jsonrpc/errors.h
@@ -0,0 +1,90 @@
+/**
+ * @file errors.h
+ * @date 31.12.2012
+ * @author Peter Spiess-Knafl <peter.knafl@gmail.com>
+ * @brief This class contains all JSON-RPC Errors as defined by standard and additional
+ * error codes for this library. It is implemented as Singleton.
+ */
+
+#ifndef ERRORS_H_
+#define ERRORS_H_
+
+#include <map>
+#include <string>
+
+#include <json/json.h>
+
+/**
+ * Defines the range for custom Server Error Codes
+ * @see http://groups.google.com/group/json-rpc/web/json-rpc-1-2-proposal?pli=1#error-object
+ */
+#define SPECIFIC_ERROR_START -32000
+/**
+ * Defines the range for custom Server Error Codes
+ * @see http://groups.google.com/group/json-rpc/web/json-rpc-1-2-proposal?pli=1#error-object
+ */
+#define SPECIFIC_ERROR_STOP -32099
+
+#define KEY_ERROR_CODE "code"
+#define KEY_ERROR_MESSAGE "message"
+
+/**
+ * Constant to represent, that there is no ERROR.
+ */
+#define ERROR_NO 0
+
+/**
+ * Official JSON-RPC 2.0 Errors
+ */
+#define ERROR_JSON_PARSE_ERROR -32700
+#define ERROR_INVALID_JSON_REQUEST -32600
+#define ERROR_METHOD_NOT_FOUND -32601
+#define ERROR_INVALID_PARAMS -32602
+#define ERROR_INTERNAL_ERROR -32603
+
+/**
+ * Specific JSON-RPC 2.0 Errors
+ */
+#define ERROR_PROCEDURE_IS_METHOD -32604
+#define ERROR_PROCEDURE_IS_NOTIFICATION -32605
+#define ERROR_PROCEDURE_PARSE_ERROR -32606
+#define ERROR_PROCEDURE_POINTER_IS_NULL -32607
+#define ERROR_PERMISSION_DENIED -32608
+#define ERROR_MALLFORMED_AUTHENTICATION_HEADER -32609
+#define ERROR_PARSING_JSON -32610
+
+namespace jsonrpc
+{
+    
+    class Errors
+    {
+        public:
+            virtual ~Errors();
+
+            /**
+             * @return This method returns the singleton instance of JSON-RPC Errors class.
+             */
+            static Errors* GetInstance();
+
+            /**
+            * @param errorCode - the code of the new error, must be between SPECIFIC_ERROR_START and SPECIFIC_ERROR_STOP
+            * @param errorMessage - the string that will be included to the errorBlock
+            * @return true if the errorCode was valid (inside SPECIFIC_ERROR Range), false otherwise.
+            */
+            bool AddError(int errorCode, const std::string& errorMessage);
+
+            /**
+             * @param errorCode - the errorCode of which you want to create an errorBlock
+             * @return - returns an error message as described in JSON-RPC 2.0 e.g. \code {"code" : -32600, "message": "INVALID_JSON_REQUEST: The JSON sent is not a valid JSON-RPC Request object" } \endcode
+             * @pre the errorcode must be known, because it is not checked.
+             * @see http://groups.google.com/group/json-rpc/web/json-rpc-1-2-proposal?pli=1#error-object
+             */
+            Json::Value GetErrorBlock(int errorCode);
+
+        private:
+            Errors();
+            std::map<int, std::string> possibleErrors;
+    };
+
+} /* namespace jsonrpc */
+#endif /* ERRORS_H_ */
