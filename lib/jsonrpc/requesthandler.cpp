e1a78c2e32c13efaa680cca8f0c9b82c3df5475e
Peter Spiess-Knafl <peter.knafl@bitmovin.net>
2012-12-31 03:01:46 +0100

    added requesthandler

---
+++ lib/jsonrpc/requesthandler.cpp
@@ -0,0 +1,289 @@
+/**
+ * @file requesthandler.cpp
+ * @date 31.12.2012
+ * @author Peter Spiess-Knafl <peter.knafl@gmail.com>
+ * @brief to be defined
+ */
+
+#include "requesthandler.h"
+#include "errors.h"
+
+using namespace std;
+
+namespace jsonrpc
+{
+    //TODO: Error Handling
+    void createErrorBlock(int errorCode, const Json::Value& request,
+            Json::Value& errorBlock)
+    {
+        errorBlock[KEY_REQUEST_VERSION] = JSON_RPC_VERSION;
+        if (!request.isNull() && request.isMember(KEY_REQUEST_ID))
+        {
+            errorBlock[KEY_REQUEST_ID] = request[KEY_REQUEST_ID];
+        }
+        else if (request.isNull())
+        {
+            errorBlock[KEY_REQUEST_ID] = Json::Value::null;
+        }
+        errorBlock[KEY_RESPONSE_ERROR] = Errors::GetInstance()->GetErrorBlock(
+                errorCode);
+    }
+
+    RequestHandler::RequestHandler(const std::string& instanceName)
+            : instanceName(instanceName), authManager(NULL)
+    {
+    }
+
+    RequestHandler::~RequestHandler()
+    {
+        for (map<string, Procedure*>::iterator it = this->procedures.begin();
+                it != this->procedures.end(); it++)
+        {
+            delete it->second;
+        }
+        if (this->authManager != NULL)
+        {
+            delete this->authManager;
+        }
+    }
+
+    void RequestHandler::AddObserver(observerFunction fp, observer_t t)
+    {
+        switch (t)
+        {
+            case ON_REQUEST:
+                this->requestObservers.push_back(fp);
+                break;
+            case ON_RESPONSE:
+                this->responseObservers.push_back(fp);
+                break;
+            case ON_REQUEST_RESPONSE:
+                this->requestObservers.push_back(fp);
+                this->responseObservers.push_back(fp);
+                break;
+        }
+    }
+
+    void RequestHandler::RemoveObserver(observerFunction fp)
+    {
+        bool found = false;
+        unsigned int i = 0;
+        while (found == false && i < this->requestObservers.size())
+        {
+            if (this->requestObservers.at(i) == fp)
+            {
+                found = true;
+                this->requestObservers.erase(
+                        this->requestObservers.begin() + i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        found = false;
+        while (found == false && i < this->responseObservers.size())
+        {
+            if (this->responseObservers.at(i) == fp)
+            {
+                found = true;
+                this->responseObservers.erase(
+                        this->responseObservers.begin() + i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    bool RequestHandler::AddProcedure(Procedure* procedure)
+    {
+        if (procedure != NULL)
+        {
+            this->procedures[procedure->GetProcedureName()] = procedure;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool RequestHandler::RemoveProcedure(const std::string& procedure)
+    {
+        map<string, Procedure*>::iterator it = this->procedures.find(procedure);
+        if (it != this->procedures.end())
+        {
+            this->procedures.erase(it);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    const AuthenticationManager& RequestHandler::GetAuthManager() const
+    {
+        return *this->authManager;
+    }
+
+    const std::string& RequestHandler::GetInstanceName() const
+    {
+        return this->instanceName;
+    }
+
+    const std::vector<observerFunction>& RequestHandler::GetResponseObservers() const
+    {
+        return this->responseObservers;
+    }
+
+    const std::vector<observerFunction>& RequestHandler::GetRequestObservers() const
+    {
+        return this->requestObservers;
+    }
+
+    const procedurelist_t& RequestHandler::GetProcedures() const
+    {
+        return this->procedures;
+    }
+
+    void RequestHandler::SetAuthManager(AuthenticationManager* authManager)
+    {
+        this->authManager = authManager;
+    }
+
+    void RequestHandler::SetProcedures(const procedurelist_t& procedures)
+    {
+        this->procedures = procedures;
+    }
+
+    void RequestHandler::HandleRequest(const std::string& request,
+            std::string& retValue)
+    {
+        Json::Reader reader;
+        Json::Value req;
+        Json::Value response, resp;
+        Json::FastWriter w;
+        int error;
+
+        //  cout << "Request was: " << request << endl;
+
+        if (reader.parse(request, req))
+        {
+            this->NotifyObservers(this->requestObservers, req);
+            //It could be a Batch Request
+            if (req.isArray())
+            {
+                for (unsigned int i = 0; i < req.size(); i++)
+                {
+                    error = this->ValidateRequest(req[i]);
+                    if (error == ERROR_NO)
+                    {
+                        this->ProcessRequest(req[i], resp);
+                        if (!resp.isNull())
+                        {
+                            response[i] = resp;
+                        }
+                    }
+                    else
+                    {
+                        createErrorBlock(error, req[i], resp);
+                        response[i] = resp;
+                    }
+                }
+                //It could be a simple Request
+            }
+            else if (req.isObject())
+            {
+                //cout << "validating request " << endl;
+                error = this->ValidateRequest(req);
+                if (error == ERROR_NO)
+                {
+                    //cout << "processing request" << endl;
+                    this->ProcessRequest(req, response);
+                }
+                else
+                {
+                    createErrorBlock(error, req, response);
+                }
+            }
+        }
+        else
+        {
+            createErrorBlock(ERROR_JSON_PARSE_ERROR, Json::Value::null,
+                    response);
+        }
+        retValue = w.write(response);
+        this->NotifyObservers(this->requestObservers, req);
+    }
+
+    int RequestHandler::ValidateRequest(const Json::Value& request)
+    {
+        int error = ERROR_NO;
+        Procedure* proc;
+        if (!(request.isMember(KEY_REQUEST_METHODNAME)
+                && request.isMember(KEY_REQUEST_VERSION)
+                && request.isMember(KEY_REQUEST_PARAMETERS)))
+        {
+            error = ERROR_INVALID_JSON_REQUEST;
+        }
+        else
+        {
+            map<string, Procedure*>::iterator it = procedures.find(
+                    request[KEY_REQUEST_METHODNAME].asString());
+
+            if (it != this->procedures.end())
+            {
+                proc =
+                        this->procedures[request[KEY_REQUEST_METHODNAME].asString()];
+                error = proc->ValdiateParameters(
+                        request[KEY_REQUEST_PARAMETERS]);
+                if (error == ERROR_NO)
+                {
+                    if (proc->GetMethodPointer() == NULL
+                            && proc->GetNotificationPointer() == NULL)
+                    {
+                        error = ERROR_PROCEDURE_POINTER_IS_NULL;
+                    }
+                    else if (request.isMember(KEY_REQUEST_ID))
+                    {
+                        if (proc->GetProcedureType() == RPC_NOTIFICATION)
+                        {
+                            error = ERROR_PROCEDURE_IS_NOTIFICATION;
+                        }
+                    }
+                    else if (proc->GetProcedureType() == RPC_METHOD)
+                    {
+                        error = ERROR_PROCEDURE_IS_METHOD;
+                    }
+                    if (this->authManager != NULL)
+                    {
+                        error = this->authManager->CheckPermission(
+                                request[KEY_AUTHENTICATION],
+                                proc->GetProcedureName());
+                    }
+                }
+            }
+            else
+            {
+                error = ERROR_METHOD_NOT_FOUND;
+            }
+        }
+        return error;
+    }
+
+    void RequestHandler::ProcessRequest(const Json::Value& request,
+            Json::Value& retValue)
+    {
+    }
+
+    void RequestHandler::NotifyObservers(
+            const std::vector<observerFunction>& observerGroup,
+            const Json::Value& request)
+    {
+    }
+
+} /* namespace jsonrpc */
+
---
+++ lib/jsonrpc/requesthandler.h
@@ -0,0 +1,123 @@
+/**
+ * @file requesthandler.h
+ * @date 31.12.2012
+ * @author Peter Spiess-Knafl <peter.knafl@gmail.com>
+ * @brief to be defined
+ */
+
+#ifndef REQUESTHANDLER_H_
+#define REQUESTHANDLER_H_
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "procedure.h"
+#include "authenticationmanager.h"
+
+#define KEY_REQUEST_METHODNAME "method"
+#define KEY_REQUEST_VERSION "jsonrpc"
+#define KEY_REQUEST_ID "id"
+#define KEY_REQUEST_PARAMETERS "params"
+#define KEY_RESPONSE_ERROR "error"
+#define KEY_RESPONSE_RESULT "result"
+#define KEY_AUTHENTICATION "auth"
+
+#define JSON_RPC_VERSION 2.0
+
+namespace jsonrpc
+{
+    /**
+     * typedef for observerCallback Functions
+     */
+    typedef void (*observerFunction)(const std::string&, const Json::Value&);
+
+    /**
+     * typedef for different Observer Types
+     */
+    typedef enum
+    {
+        ON_RESPONSE, ON_REQUEST, ON_REQUEST_RESPONSE
+    } observer_t;
+    
+    typedef std::map<std::string, Procedure*> procedurelist_t;
+
+    class RequestHandler
+    {
+        public:
+            RequestHandler(const std::string& instanceName);
+            virtual ~RequestHandler();
+
+            /**
+             * @param fp - the passed function is called on every request that comes in.
+             * @param t - sets the type of this observer.
+             */
+            void AddObserver(observerFunction fp, observer_t t);
+            void RemoveObserver(observerFunction fp);
+
+            bool AddProcedure(Procedure* procedure);
+            bool RemoveProcedure(const std::string& procedure);
+
+            const AuthenticationManager& GetAuthManager() const;
+            const std::string& GetInstanceName() const;
+
+            const std::vector<observerFunction>& GetResponseObservers() const;
+            const std::vector<observerFunction>& GetRequestObservers() const;
+            const procedurelist_t& GetProcedures() const;
+
+            void SetAuthManager(AuthenticationManager* authManager);
+            void SetProcedures(const procedurelist_t& procedures);
+
+            /**
+             * This is the key feature of this class, it deals with the JSOn-RPC 2.0 protocol.
+             *  @param request - holds (hopefully) a valid JSON-Request Object.
+             *  @param retValue a reference to string object which will hold the response after this method;
+             */
+            void HandleRequest(const std::string& request, std::string& retValue);
+
+        private:
+
+            int ValidateRequest(const Json::Value &val);
+
+            /**
+             * @pre the request must be a valid request
+             * @param request - the request Object compliant to Json-RPC 2.0
+             * @param retValue - a reference to an object which will hold the returnValue afterwards.
+             *
+             * after calling this method, the requested Method will be executed. It is important, that this method only gets called once per request.
+             */
+            void ProcessRequest(const Json::Value &request,
+                    Json::Value &retValue);
+
+            /**
+             * This method is called on each request, to notify all Registered Observers.
+             */
+            void NotifyObservers(
+                    const std::vector<observerFunction>& observerGroup,
+                    const Json::Value& request);
+
+            /**
+             * Each Request Handler has its instancename to identify for logging purposes.
+             */
+            std::string instanceName;
+
+            /**
+             * This vector holds all observers that want to be notified on each request.
+             */
+            std::vector<observerFunction> requestObservers;
+            std::vector<observerFunction> responseObservers;
+
+            /**
+             * This map holds all procedures. The string holds the name of each procedure.
+             */
+            procedurelist_t procedures;
+
+            /**
+             * this objects decides whether a request is allowed to be processed or not.
+             */
+            AuthenticationManager* authManager;
+
+    };
+
+} /* namespace jsonrpc */
+#endif /* REQUESTHANDLER_H_ */
