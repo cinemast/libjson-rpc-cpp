54888ef60a7662270c4b4b8e3d88008d19fb7519
Peter Spiess-Knafl <peter.knafl@gmail.com>
2013-06-03 21:32:28 +0200

    try to fix linux build

--- src/example/xbmcremote.cpp
+++ src/example/xbmcremote.cpp
@@ -67,26 +67,21 @@ int kbhit_mac() {
 #else
 //Taken from: http://stackoverflow.com/questions/2984307/c-key-pressed-in-linux-console
 int kbhit_linux() {
-    int character;
-    struct termios orig_term_attr;
-    struct termios new_term_attr;
-
-    /* set the terminal to raw mode */
-    tcgetattr(fileno(stdin), &orig_term_attr);
-    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-    new_term_attr.c_lflag &= ~(ECHO|ICANON);
-    new_term_attr.c_cc[VTIME] = 0;
-    new_term_attr.c_cc[VMIN] = 0;
-    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
-
-    /* read a character from the stdin stream without blocking */
-    /*   returns EOF (-1) if no character is available */
-    character = fgetc(stdin);
-
-    /* restore the original terminal attributes */
-    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
-
-    return character;
+    int c=0;
+    struct termios org_opts, new_opts;
+    int res=0;
+    //-----  store old settings -----------
+    res=tcgetattr(STDIN_FILENO, &org_opts);
+    assert(res==0);
+    //---- set new terminal parms --------
+    memcpy(&new_opts, &org_opts, sizeof(new_opts));
+    new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+    c=getchar();
+    //------  restore old settings ---------
+    res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+    assert(res==0);
+    return(c);
 }
 #endif
 int main(int argc, char** argv) {
@@ -128,8 +123,5 @@ int main(int argc, char** argv) {
             cerr << e.what() << endl;
         }
     }
-
-
-
     return 0;
 }
