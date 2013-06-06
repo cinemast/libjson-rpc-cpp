bcee78392295a4b1ce24397568cb813a422f0c4f
Peter Spiess-Knafl <peter.knafl@gmail.com>
2013-06-06 23:13:08 +0200

    gitignore for dist folder

--- dev/createpackage.sh
+++ dev/createpackage.sh
@@ -3,13 +3,12 @@
 	echo "Bundling for $1"
 	mkdir -p ../dist/libjson-rpc-cpp
 	mkdir -p ../dist/libjson-rpc-cpp/doc
-	cd ../dist/doc
-	doxygen ../../doc/libjson-rpc-cpp.doxyfile
-	cd ../../
-	cd build
-	cmake -DCMAKE_INSTALL_PREFIX=../dist/libjson-rpc-cpp
-	make install
-	cd ../dist
-	zip -r libjson-rpc-cpp-$1.zip json-rpc-cpp
+	cd ../doc
+	doxygen libjson-rpc-cpp.doxyfile
+	mv html/* ../dist/libjson-rpc-cpp/doc
+	#cmake -DCMAKE_INSTALL_PREFIX=../dist/libjson-rpc-cpp
+	#make install
+	#cd ../dist
+	zip -r libjson-rpc-cpp-$1.zip libjson-rpc-cpp
 	rm -r libjson-rpc-cpp
 	cd ../dev
