6bdb437df065be639008a6e428d393404791a3f2
Peter Spiess-Knafl <peter.knafl@gmail.com>
2014-10-26 12:26:50 +0000

    adapting travis build

---
+++ dev/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+mkdir -p build
+cd build
+cmake ..
+make
+make test
+sudo make install
+sudo ldconfig
+g++ ../src/examples/simpleclient.cpp -ljsonrpccpp-client -ljsoncpp -ljsonrpccpp-common -lcurl -o sampleclient 
+g++ ../src/examples/simpleserver.cpp -ljsonrpccpp-server -ljsoncpp -ljsonrpccpp-common -o sampleserver
+
