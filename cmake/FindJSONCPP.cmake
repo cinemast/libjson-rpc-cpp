2c6c40a4c0586e239f05e5eb95fea53082b29d20
Peter Spiess-Knafl <peter.knafl@gmail.com>
2014-11-06 00:16:26 +0000

    adapted cmake find scripts

--- cmake/FindJSONCPP.cmake
+++ cmake/FindJSONCPP.cmake
@@ -1,47 +1,40 @@
-# -*- cmake -*-
-# - Find JSONCpp
-# Find the JSONCpp includes and library
-# This module defines
-#  JSONCPP_INCLUDE_DIRS, where to find json.h, etc.
-#  JSONCPP_LIBRARIES, the libraries needed to use jsoncpp.
-#  JSONCPP_FOUND, If false, do not try to use jsoncpp.
-#  also defined, but not for general use are
-#  JSONCPP_LIBRARIES, where to find the jsoncpp library.
+# - Try to find JSONCPP
+# Once done this will define
+#
+#  JSONCPP_FOUND - system has JSONCPP
+#  JSONCPP_INCLUDE_DIRS - the JSONCPP include directory
+#  JSONCPP_LIBRARY - Link these to use JSONCPP
 
-FIND_PATH(JSONCPP_INCLUDE_DIRS json.h
-/usr/include
-/usr/local/include
-${CMAKE_SOURCE_DIR}/win32-deps/include/jsoncpp/json
-PATH_SUFFIXES jsoncpp/json jsoncpp
+FIND_LIBRARY (JSONCPP_LIBRARIES NAMES jsoncpp
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    ${CMAKE_SOURCE_DIR}/win32-deps/lib
 )
 
+FIND_PATH (JSONCPP_INCLUDE_DIRS json.h
+    PATHS
+    /usr/include
+    /usr/local/include
+    ${CMAKE_SOURCE_DIR}/win32-deps/include
+    PATH_SUFFIXES jsoncpp/json json
+)
 
-FIND_LIBRARY(JSONCPP_LIBRARIES NAMES jsoncpp HINTS /usr/lib /usr/local/lib 	${CMAKE_SOURCE_DIR}/win32-deps/lib)
+IF(JSONCPP_INCLUDE_DIRS AND JSONCPP_LIBRARIES)
+    SET(JSONCPP_FOUND TRUE)
+ENDIF(JSONCPP_INCLUDE_DIRS AND JSONCPP_LIBRARIES)
 
-IF (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
-    SET(JSONCPP_LIBRARIES ${JSONCPP_LIBRARIES})
-    SET(JSONCPP_FOUND "YES")
-ELSE (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
-  SET(JSONCPP_FOUND "NO")
-ENDIF (JSONCPP_LIBRARIES AND JSONCPP_INCLUDE_DIRS)
-
-
-IF (JSONCPP_FOUND)
-   IF (NOT JSONCPP_FIND_QUIETLY)
-      MESSAGE(STATUS "Found JSONCpp: ${JSONCPP_LIBRARIES}")
-   ENDIF (NOT JSONCPP_FIND_QUIETLY)
-ELSE (JSONCPP_FOUND)
-   IF (JSONCPP_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find JSONCPP library include: ${JSONCPP_INCLUDE_DIRS}, lib: ${JSONCPP_LIBRARIES}")
-   ENDIF (JSONCPP_FIND_REQUIRED)
-ENDIF (JSONCPP_FOUND)
-
-# Deprecated declarations.
-SET (NATIVE_JSONCPP_INCLUDE_PATH ${JSONCPP_INCLUDE_DIRS} )
-GET_FILENAME_COMPONENT (NATIVE_JSONCPP_LIB_PATH ${JSONCPP_LIBRARIES} PATH)
+IF(JSONCPP_FOUND)
+    IF(NOT JSONCPP_FIND_QUIETLY)
+        MESSAGE(STATUS "Found libjsoncpp: ${JSONCPP_LIBRARIES}")
+    ENDIF(NOT JSONCPP_FIND_QUIETLY)
+ELSE(JSONCPP_FOUND)
+    IF(JSONCPP_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find libjsoncpp")
+    ENDIF(JSONCPP_FIND_REQUIRED)
+ENDIF(JSONCPP_FOUND)
 
 MARK_AS_ADVANCED(
-  JSONCPP_LIBRARIES
-  JSONCPP_INCLUDE_DIRS
-  )
-
+    JSONCPP_LIBRARIES
+    JSONCPP_INCLUDE_DIRS
+)
---
+++ cmake/FindMHD.cmake
@@ -0,0 +1,39 @@
+# - Try to find MHD
+# Once done this will define
+#
+#  MHD_FOUND - system has MHD
+#  MHD_INCLUDE_DIRS - the MHD include directory
+#  MHD_LIBRARY - Link these to use MHD
+
+FIND_LIBRARY (MHD_LIBRARIES NAMES microhttpd
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    ${CMAKE_SOURCE_DIR}/win32-deps/lib
+)
+
+FIND_PATH (MHD_INCLUDE_DIRS microhttpd.h
+    PATHS
+    /usr/include
+    /usr/local/include
+    ${CMAKE_SOURCE_DIR}/win32-deps/include
+)
+
+IF(MHD_INCLUDE_DIRS AND MHD_LIBRARIES)
+    SET(MHD_FOUND TRUE)
+ENDIF(MHD_INCLUDE_DIRS AND MHD_LIBRARIES)
+
+IF(MHD_FOUND)
+    IF(NOT MHD_FIND_QUIETLY)
+        MESSAGE(STATUS "Found libmicrohttpd: ${MHD_LIBRARIES}")
+    ENDIF(NOT MHD_FIND_QUIETLY)
+ELSE(MHD_FOUND)
+    IF(MHD_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find libmicrohttpd")
+    ENDIF(MHD_FIND_REQUIRED)
+ENDIF(MHD_FOUND)
+
+MARK_AS_ADVANCED(
+    MHD_LIBRARIES
+    MHD_INCLUDE_DIRS
+)
