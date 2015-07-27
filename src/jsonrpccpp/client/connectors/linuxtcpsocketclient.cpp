e91d8603fd6b386f2f3934156ba5a267c3dd61ee
Alexandre Poirot <alexandre.poirot@legrand.fr>
2015-07-27 12:06:40 +0200

    Improved errors handling on linux tcp socket client implementation. Now throw a ERROR_CLIENT_CONNECTOR which is more fit to errors involved by socket manipulation than ERROR_RPC_ERROR.

--- src/jsonrpccpp/client/connectors/linuxtcpsocketclient.cpp
+++ src/jsonrpccpp/client/connectors/linuxtcpsocketclient.cpp
@@ -15,6 +15,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iostream>
+#include <errno.h>
+#include <cstring>
 
 #define BUFFER_SIZE 64
 #ifndef DELIMITER_CHAR
@@ -42,8 +44,32 @@ void LinuxTcpSocketClient::SendRPCMessage(const std::string& message, std::strin
 	char buffer[BUFFER_SIZE];
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd < 0) {
-		cerr << "Socket failed" << endl;
-		throw JsonRpcException(Errors::ERROR_RPC_INTERNAL_ERROR, result);
+		string message = "socket() failed";
+                switch(errno) {
+                    case EACCES:
+                        message = strerror(EACCES);
+                        break;
+                    case EAFNOSUPPORT:
+                        message = strerror(EAFNOSUPPORT);
+                        break;
+                    case EINVAL:
+                        message = strerror(EINVAL);
+                        break;
+                    case EMFILE:
+                        message = strerror(EMFILE);
+                        break;
+                    case ENOBUFS:
+                        message = strerror(ENOBUFS);
+                        break;
+                    case ENOMEM:
+                        message = strerror(ENOMEM);
+                        break;
+                    case EPROTONOSUPPORT:
+                        message = strerror(EPROTONOSUPPORT);
+                        break;
+                }
+                cerr << message << endl;
+                throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR, message);
 	}
 
 	memset(&address, 0, sizeof(sockaddr_in));
@@ -53,15 +79,119 @@ void LinuxTcpSocketClient::SendRPCMessage(const std::string& message, std::strin
 	address.sin_port = htons(this->port);
 
 	if(connect(socket_fd, (struct sockaddr *) &address,  sizeof(sockaddr_in)) != 0) {
-		cerr << "connect failed" << endl;
-		throw JsonRpcException(Errors::ERROR_RPC_INTERNAL_ERROR, result);
+		string message = "connect() failed";
+		switch(errno) {
+                    case EACCES:
+                        message = strerror(EACCES);
+                        break;
+                    case EPERM:
+                        message = strerror(EPERM);
+                        break;
+                    case EADDRINUSE:
+                        message = strerror(EADDRINUSE);
+                        break;
+                    case EAFNOSUPPORT:
+                        message = strerror(EAFNOSUPPORT);
+                        break;
+                    case EAGAIN:
+                        message = strerror(EAGAIN);
+                        break;
+                    case EALREADY:
+                        message = strerror(EALREADY);
+                        break;
+                    case EBADF:
+                        message = strerror(EBADF);
+                        break;
+                    case ECONNREFUSED:
+                        message = strerror(ECONNREFUSED);
+                        break;
+                    case EFAULT:
+                        message = strerror(EFAULT);
+                        break;
+                    case EINPROGRESS:
+                        message = strerror(EINPROGRESS);
+                        break;
+                    case EINTR:
+                        message = strerror(EINTR);
+                        break;
+                    case EISCONN:
+                        message = strerror(EISCONN);
+                        break;
+                    case ENETUNREACH:
+                        message = strerror(ENETUNREACH);
+                        break;
+                    case ENOTSOCK:
+                        message = strerror(ENOTSOCK);
+                        break;
+                    case ETIMEDOUT:
+                        message = strerror(ETIMEDOUT);
+                        break;
+                }
+                cerr << message << endl;
+                throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR, message);
 	}
 
 	bool fullyWritten = false;
 	string toSend = message;
 	do {
 		ssize_t byteWritten = send(socket_fd, toSend.c_str(), toSend.size(), 0);
-		if(byteWritten < toSend.size()) {
+		if(byteWritten == -1) {
+                    string message = "send() failed";
+                    switch(errno) {
+                        case EACCES:
+                            message = strerror(EACCES);
+                            break;
+                        case EWOULDBLOCK:
+                            message = strerror(EWOULDBLOCK);
+                            break;
+                        case EBADF:
+                            message = strerror(EBADF);
+                            break;
+                        case ECONNRESET:
+                            message = strerror(ECONNRESET);
+                            break;
+                        case EDESTADDRREQ:
+                            message = strerror(EDESTADDRREQ);
+                            break;
+                        case EFAULT:
+                            message = strerror(EFAULT);
+                            break;
+                        case EINTR:
+                            message = strerror(EINTR);
+                            break;
+                        case EINVAL:
+                            message = strerror(EINVAL);
+                            break;
+                        case EISCONN:
+                            message = strerror(EISCONN);
+                            break;
+                        case EMSGSIZE:
+                            message = strerror(EMSGSIZE);
+                            break;
+                        case ENOBUFS:
+                            message = strerror(ENOBUFS);
+                            break;
+                        case ENOMEM:
+                            message = strerror(ENOMEM);
+                            break;
+                        case ENOTCONN:
+                            message = strerror(ENOTCONN);
+                            break;
+                        case ENOTSOCK:
+                            message = strerror(ENOTSOCK);
+                            break;
+                        case EOPNOTSUPP:
+                            message = strerror(EOPNOTSUPP);
+                            break;
+                        case EPIPE:
+                            message = strerror(EPIPE);
+                            break;
+                    }
+                    close(socket_fd);
+                    cerr << message << endl;
+                    throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR, message);
+                }
+                else if(byteWritten < toSend.size()) {
 			int len = toSend.size() - byteWritten;
 			toSend = toSend.substr(byteWritten + sizeof(char), len);
 		}
@@ -71,10 +201,46 @@ void LinuxTcpSocketClient::SendRPCMessage(const std::string& message, std::strin
 
 	do {
 		nbytes = recv(socket_fd, buffer, BUFFER_SIZE, 0);
-		string tmp;
-		tmp.append(buffer, nbytes);
-		result.append(buffer,nbytes);
-
+                if(nbytes == -1) {
+                    string message = "recv() failed";
+                    switch(errno) {
+                        case EWOULDBLOCK:
+                            message = strerror(EWOULDBLOCK);
+                            break;
+                        case EBADF:
+                            message = strerror(EBADF);
+                            break;
+                        case ECONNRESET:
+                            message = strerror(ECONNRESET);
+                            break;
+                        case EFAULT:
+                            message = strerror(EFAULT);
+                            break;
+                        case EINTR:
+                            message = strerror(EINTR);
+                            break;
+                        case EINVAL:
+                            message = strerror(EINVAL);
+                            break;
+                        case ENOMEM:
+                            message = strerror(ENOMEM);
+                            break;
+                        case ENOTCONN:
+                            message = strerror(ENOTCONN);
+                            break;
+                        case ENOTSOCK:
+                            message = strerror(ENOTSOCK);
+                            break;
+                    }
+                    close(socket_fd);
+                    cerr << message << endl;
+                    throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR, message);
+                }
+                else {
+                    string tmp;
+                    tmp.append(buffer, nbytes);
+                    result.append(buffer,nbytes);
+                }
 	} while(result.find(DELIMITER_CHAR) == string::npos);
 
 	close(socket_fd);
