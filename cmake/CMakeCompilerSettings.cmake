2fd467eb5d02964494fd6c48cac7a7818370964a
Eric Lagergren <ericscottlagergren@gmail.com>
2015-11-27 22:02:17 -0800

    Added gcc5 patch + support for IDs over (1 << 32 - 2) / 1

--- cmake/CMakeCompilerSettings.cmake
+++ cmake/CMakeCompilerSettings.cmake
@@ -1,7 +1,8 @@
 # Set necessary compile and link flags
 
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
-	set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wextra -Wnon-virtual-dtor -fprofile-arcs -ftest-coverage -fPIC -O0")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
+	set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11 -Wall -Wextra -Wnon-virtual-dtor -fprofile-arcs -ftest-coverage -fPIC -O0")
 elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
 # TODO figure clang stuff to enable test-coverage
 # Instrument Program flow should be set to Yes
