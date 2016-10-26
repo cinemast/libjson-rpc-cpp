c48809e19385ef12107e27e397837cb0c67e395a
JD <jeandaniel.michaud@ge.com>
2016-10-26 09:12:12 +0200

    Introduce the file descriptor client connector.

---
+++ src/jsonrpccpp/client/connectors/filedescriptorclient.cpp
@@ -0,0 +1,95 @@
+/*************************************************************************
+ * libjson-rpc-cpp
+ *************************************************************************
+ * @file    filedescriptorclient.cpp
+ * @date    26.10.2016
+ * @author  Jean-Daniel Michaud <jean.daniel.michaud@gmail.com>
+ * @license See attached LICENSE.txt
+ ************************************************************************/
+
+#include "filedescriptorclient.h"
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+#define BUFFER_SIZE 64
+#ifndef DELIMITER_CHAR
+#define DELIMITER_CHAR char(0x0A)
+#endif //DELIMITER_CHAR
+#define MEX_ERR_MSG 255
+
+using namespace jsonrpc;
+using namespace std;
+
+FileDescriptorClient::FileDescriptorClient(int inputfd, int outputfd) :
+  inputfd(inputfd), inputfd(outputfd)
+{
+}
+
+FileDescriptorClient::~FileDescriptorClient()
+{
+}
+
+void FileDescriptorClient::SendRPCMessage(const std::string& message,
+  std::string& result) throw (JsonRpcException)
+{
+  if (!IsWritable(outputfd))
+    throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR,
+      "The output file descriptor is not writable");
+
+  bool fullyWritten = false;
+  string toSend = message;
+  do
+  {
+    ssize_t byteWritten = write(outputfd, toSend.c_str(), toSend.size());
+    if (byteWritten < 1) {
+      char errorstr[MEX_ERR_MSG];
+      if (strerror_r(errno, errorstr, MEX_ERR_MSG) == 0)
+        throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR, std::string(errorstr));
+      else
+        throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR,
+          "Unknown error occured while writing to the output file descriptor");
+    }
+
+    if (byteWritten < toSend.size())
+    {
+      int len = toSend.size() - byteWritten;
+      toSend = toSend.substr(byteWritten + sizeof(char), len);
+    }
+    else
+      fullyWritten = true;
+  } while(!fullyWritten);
+
+  if (!IsReadable(inputfd))
+    throw JsonRpcException(Errors::ERROR_CLIENT_CONNECTOR,
+      "The input file descriptor is not readable");
+
+  do
+  {
+    nbytes = read(inputfd, buffer, BUFFER_SIZE);
+    result.append(buffer, nbytes);
+  } while(result.find(DELIMITER_CHAR) == string::npos);
+}
+
+int FileDescriptorClient::IsReadable(int fd)
+{
+  int o_accmode = 0;
+  int ret = fcntl(fd, F_GETFL, &o_accmode);
+  if(ret  == -1 )
+    return ret;
+  return (((o_accmode & O_ACCMODE)) == O_RDONLY ||
+    (o_accmode & O_ACCMODE) == O_RDWR);
+}
+
+int FileDescriptorClient::IsWritable(int fd)
+{
+  int o_accmode = 0;
+  int ret = fcntl(fd, F_GETFL, &o_accmode);
+  if (ret  == -1)
+    return ret;
+  return (((o_accmode & O_ACCMODE)) == O_RDONLY ||
+    (o_accmode & O_ACCMODE) == O_RDWR);
+}
