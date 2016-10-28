6296dcf42ecff1a5fc38d391238d33e46288b6a5
JD <jean.daniel.michaud@gmail.com>
2016-10-28 19:36:19 +0200

    Move example readme content into individual example files.

--- src/examples/filedescriptorclient.cpp
+++ src/examples/filedescriptorclient.cpp
@@ -5,6 +5,29 @@
  * @brief An example implementation of a client based on plain old file descriptors
  */
 
+/*
+ * This example demonstrate the use of simple file descriptor to connect
+ * processes with jsonrpc.
+ *
+ * First create a named pipe:
+ *
+ * ```
+ * mkfifo FOO
+ * ```
+ *
+ * Then launch the client and the server by connecting them to each other
+ * this way:
+ * ```
+ * filedescriptorclientsample < PIPE | filedescriptorserversample > PIPE
+ * ```
+ *
+ * The way the standard output of the client is connected to the standard
+ * input of the server, and /vice versa/.
+ *
+ * You can even launch the server in standalone and enter json-rpc
+ * compliant json string in the standard input to test it.
+ */
+
 #include <jsonrpccpp/client.h>
 #include <jsonrpccpp/client/connectors/filedescriptorclient.h>
 #include <iostream>
