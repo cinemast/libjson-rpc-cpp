8bfc0e513275db1fc219f32ec11fd7ed6cf75d29
Peter Spiess-Knafl <dev@spiessknafl.at>
2017-04-09 09:58:30 +0200

    Fix build for arch

--- docker/build_test_install.sh
+++ docker/build_test_install.sh
@@ -1,15 +1,15 @@
 set -v
+set -e
 
 CLIENT_LIBS="-ljsoncpp -lcurl -ljsonrpccpp-common -ljsonrpccpp-client"
 SERVER_LIBS="-ljsoncpp -lmicrohttpd -ljsonrpccpp-common -ljsonrpccpp-server"
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=ON \
--DBUILD_SHARED_LIBS=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=ON ..
 make -j$(nproc)
 make test
 make install
 ldconfig
-ls -l /usr/local/lib
 cd ../src/examples
 g++ simpleclient.cpp $CLIENT_LIBS -o simpleclient
 g++ simpleserver.cpp $SERVER_LIBS -o simpleserver
