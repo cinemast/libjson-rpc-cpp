8738bd02652204a01d429aa10533ea14ae7572c4
Peter Spiess-Knafl <peter@technikrabe.at>
2017-08-24 23:33:56 +0200

    Enable coverage

--- docker/build_test_install.sh
+++ docker/build_test_install.sh
@@ -4,7 +4,7 @@ set -e
 CLIENT_LIBS="-ljsoncpp -lcurl -ljsonrpccpp-common -ljsonrpccpp-client"
 SERVER_LIBS="-ljsoncpp -lmicrohttpd -ljsonrpccpp-common -ljsonrpccpp-server"
 mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_COVERAGE=YES -DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_STATIC_LIBS=ON -DTCP_SOCKET_SERVER=YES -DTCP_SOCKET_CLIENT=YES \
 	-DBUILD_SHARED_LIBS=ON ..
 make -j$(nproc)
