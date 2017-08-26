13fe59e68757c8f780022f82050cbd23502fd155
Peter Spiess-Knafl <peter.knafl@gmail.com>
2017-08-26 18:37:49 +0200

    Added Debian buster to CI

--- docker/build_test_install.sh
+++ docker/build_test_install.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
 set -v
 set -e
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]
+then
+	PREFIX=/usr/local
+else
+	PREFIX=/usr
+fi
 
 CLIENT_LIBS="-ljsoncpp -lcurl -ljsonrpccpp-common -ljsonrpccpp-client -lhiredis"
 SERVER_LIBS="-ljsoncpp -lmicrohttpd -ljsonrpccpp-common -ljsonrpccpp-server -lhiredis"
 mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_COVERAGE=YES -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DWITH_COVERAGE=YES -DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_STATIC_LIBS=ON -DTCP_SOCKET_SERVER=YES -DTCP_SOCKET_CLIENT=YES \
 	-DBUILD_SHARED_LIBS=ON ..
 make -j$(nproc)
@@ -13,7 +21,10 @@ make -j$(nproc)
 if [ "$OS" == "native" ]
 then
 	sudo make install
-	sudo ldconfig
+	if [ "$TRAVIS_OS_NAME" != "osx" ]
+	then
+		sudo ldconfig
+	fi
 else
 	make install
 	ldconfig
@@ -22,7 +33,7 @@ cd ../src/examples
 g++ -std=c++11 simpleclient.cpp $CLIENT_LIBS -o simpleclient
 g++ -std=c++11 simpleserver.cpp $SERVER_LIBS -o simpleserver
 
-mkdir gen && cd gen
+mkdir -p gen && cd gen
 jsonrpcstub ../spec.json --cpp-server=AbstractStubServer --cpp-client=StubClient
 cd ..
 g++ -std=c++11 stubclient.cpp $CLIENT_LIBS -o stubclient
