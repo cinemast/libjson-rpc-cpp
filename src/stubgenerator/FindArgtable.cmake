3fb4c650ba8269f6232b0c3baaf934623b87bb65
Peter Spiess-Knafl <dev@spiessknafl.at>
2018-11-20 22:55:54 +0100

    compiling testcases for jsonrpcstub

--- src/stubgenerator/FindArgtable.cmake
+++ src/stubgenerator/FindArgtable.cmake
@@ -20,18 +20,6 @@ find_library (
 set(ARGTABLE_INCLUDE_DIRS ${ARGTABLE_INCLUDE_DIR})
 set(ARGTABLE_LIBRARIES ${ARGTABLE_LIBRARY})
 
-# debug library on windows
-# same naming convention as in qt (appending debug library with d)
-# boost is using the same "hack" as us with "optimized" and "debug"
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    find_library(
-        ARGTABLE_LIBRARY_DEBUG
-        NAMES argtable2d
-        DOC "argtable debug library"
-    )
-    set(ARGTABLE_LIBRARIES optimized ${ARGTABLE_LIBRARIES} debug ${ARGTABLE_LIBRARY_DEBUG})
-endif()
-
 # handle the QUIETLY and REQUIRED arguments and set JSONCPP_FOUND to TRUE
 # if all listed variables are TRUE, hide their existence from configuration view
 include(FindPackageHandleStandardArgs)
