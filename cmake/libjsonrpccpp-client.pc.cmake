a4ea121f289cd10bbd647e3aed0a3d9613d547e5
Daniel Kraft <d@domob.eu>
2020-01-09 17:20:49 +0100

    Fix include and lib directories in pc files.
    Use the correct variables (see documentation at
    https://cmake.org/cmake/help/v3.0/module/GNUInstallDirs.html) for the
    include and lib directories in the generated pkg-config files.
    
    The other variables are undefined and lead to misformed and unusable
    .pc files installed.

--- cmake/libjsonrpccpp-client.pc.cmake
+++ cmake/libjsonrpccpp-client.pc.cmake
@@ -1,5 +1,5 @@
 Name: libjsonrpccpp-client
 Description: A C++ client implementation of json-rpc.
 Version: ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}
-Libs: -L${FULL_PATH_LIBDIR} -ljsoncpp -ljsonrpccpp-common -ljsonrpccpp-client ${CLIENT_LIBS}
-Cflags: -I${FULL_PATH_INCLUDEDIR}
+Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -ljsoncpp -ljsonrpccpp-common -ljsonrpccpp-client ${CLIENT_LIBS}
+Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
--- cmake/libjsonrpccpp-common.pc.cmake
+++ cmake/libjsonrpccpp-common.pc.cmake
@@ -1,5 +1,5 @@
 Name: libjsonrpccpp-common
 Description: Common libraries for libjson-rpc-cpp
 Version: ${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}
-Libs: -L${FULL_PATH_LIBDIR} -ljsoncpp
-Cflags: -I${FULL_PATH_INCLUDEDIR}
+Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -ljsoncpp
+Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
